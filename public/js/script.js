efa39d5b4478d4acb8338c3eadcd33679122980c
Jess Gardner <inanna1130@gmail.com>
2017-07-12 22:45:25 -0400

    Remade gameboard HTML again and added question data JS.

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,83 @@
 
+$(document).ready(function(){
 
+ // question data //   
+    var QuestionSet = [
+        {
+            id: "1-1",
+            question: "question 1",
+            answers: {
+                a1: "answer1",
+                a2: "answer2",
+                a3: "answer3",
+                a4: "answer4"
+            },
+            correctAnswer: "a1",
+            points: "$100"
+        },
+        {
+            id: "1-2",
+            question: "question 2",
+            answers: {
+                a1: "answer1",
+                a2: "answer2",
+                a3: "answer3",
+                a4: "answer4"
+            },
+            correctAnswer: "a2",
+            points: "$200"
+        },
+        {
+            id: "1-3",
+            question: "question 3",
+            answers: {
+                a1: "answer1",
+                a2: "answer2",
+                a3: "answer3",
+                a4: "answer4"
+            },
+            correctAnswer: "a3",
+            points: "$300"
+        },
+        {
+            id: "1-4",
+            question: "question 4",
+            answers: {
+                a1: "answer1",
+                a2: "answer2",
+                a3: "answer3",
+                a4: "answer4"
+            },
+            correctAnswer: "a4",
+            points: "$400"
+        },
+        {
+            id: "1-5",
+            question: "question 5",
+            answers: {
+                a1: "answer1",
+                a2: "answer2",
+                a3: "answer3",
+                a4: "answer4"
+            },
+            correctAnswer: "a1",
+            points: "$500"
+        }
+    ];
 
+ // choose points button //
 
-var questionSet= {
-    question: 'Question??',
-    multipleAnswers: ['Answer 1', 'Answer 2', 'Answer 3'],
-    correctAnswer: 'Answer 2'
-}
+ // choose answer //
+
+ // answer value //
+
+ // fire modals //
+
+ // scoring //
+
+ // winner/loser //
+
+});
 
 
 
